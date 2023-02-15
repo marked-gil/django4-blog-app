aabb22912f22a02481f2f88fb380726675cacded
marked-gil <100248122+marked-gil@users.noreply.github.com>
2023-02-15 20:48:15 +0000

    Customize how models are displayed on admin page

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -5,3 +5,15 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     """ sets the model fields that gets displayed on the admin object list page """
     list_display = ['title', 'slug', 'author', 'publish', 'status']
+    """ specifies the model fields to filter results by """
+    list_filter = ['status', 'created', 'publish', 'author']
+    """ creates a search bar & defines a list of searchable fields """
+    search_fields = ['title', 'body']
+    """ prepopulates the slug field with the title field"""
+    prepopulated_fields = {'slug': ('title',)}
+    """ displays a lookup widget on the author field """
+    raw_id_fields = ['author']
+    """ creates a navigation links to navigate through a date hierarchy """
+    date_hierarchy = 'publish'
+    """ specifies the default sorting criteria"""
+    ordering = ['status', 'publish']

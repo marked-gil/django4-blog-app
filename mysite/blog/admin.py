27bbee5fe49bb88b9e531dbf76d8f6d576faf7ca
marked-gil <100248122+marked-gil@users.noreply.github.com>
2023-02-15 14:54:40 +0000

    register Post model to admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """ sets the model fields that gets displayed on the admin object list page """
+    list_display = ['title', 'slug', 'author', 'publish', 'status']

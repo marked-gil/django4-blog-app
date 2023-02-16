ca432ae2ce0a40ff282363f507f82a1a77b38e3b
marked-gil <100248122+marked-gil@users.noreply.github.com>
2023-02-16 11:37:48 +0000

    Create post_detail template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -16,7 +16,7 @@ def post_detail(request, id):
     
     post = get_object_or_404(Post,
                              id=id,
-                             status=Post.status.PUBLISHED)
+                             status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})

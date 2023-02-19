35a459227f21e65fa3e9b740f926fefaa5e3d32c
marked-gil <100248122+marked-gil@users.noreply.github.com>
2023-02-19 14:01:16 +0000

    Handle forms in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -13,7 +14,7 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     
     try:
-        # obtains the objects for desired page by calling page() method of paginator
+        # obtains objects for desired page by calling page() method of paginator
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         # If page_number is not an integer, deliver first page.
@@ -47,3 +48,28 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, 
+                             status=Post.Status.PUBLISHED)
+    
+    if request.method == 'POST':
+        # Form was submitted
+        # form instance created with submitted data (request.POST)
+        form = EmailPostForm(request.POST)
+        # validates data as per EmailPostForm specified constraints
+        # returns True if form is valid, False otherwise
+        # List of validation errors can be obtained with "form.errors"
+        if form.is_valid():
+            # retrieves validated data
+            # "cleaned_data" attribute - dictionary of form fields w/ values
+            # (cleaned_data contains only valid fields if form doesn't validate)
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        # view receives GET request when page loads for 1st time
+        form = EmailPostForm()
+    
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form})
